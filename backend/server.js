b6ee7293ac3dff6bd34a7fb3e73784a719c4e327
ayata30 <ayataadams@gmail.com>
2025-07-21 06:21:29 -0600

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
-import express from "express"
+import express from "express";
 
-const app = express() 
-app.listen(5001, () )
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+    // delete a 
+    res.status(200).send("you got 5 notes");
+});
+
+
+
+app.post("api/notes",(req, res) =>{
+    res.status(201).json({message: "post created sucessgyully!"})
+})
+
+
+app.listen(5001, () => {
+    console.log("Server started on PORT: 5001");
+});
